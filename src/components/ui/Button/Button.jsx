8fc73734bb179a62b1c9b67e85f51396273d9929
Olga Romanova <olgaromanova@MacBook-Pro-Olga.local>
2026-09-15 18:10:22 +0200

    refactor: add default values to ButtonReact props

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 
 export default function Button({
   children,
-  variant,
-  size,
+  variant = "",
+  size = "",
   as = "button",
   className = "",
   ...props
