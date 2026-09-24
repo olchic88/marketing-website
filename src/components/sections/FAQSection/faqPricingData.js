494747a4a4fc869952d1da43c892dd1f0bd2bc00
Olga Romanova <olgaromanova@MacBook-Pro-Olga.local>
2026-09-24 15:43:35 +0200

    refactor: extract FAQ data into separate files

---
+++ src/components/sections/FAQSection/faqPricingData.js
@@ -0,0 +1,39 @@
+export const faqsPricing = [
+  {
+    id: 0,
+    question: "Are there any discounts available for long-term subscriptions?",
+    answer:
+      "Yes! We offer a discount on annual subscriptions. If you choose an annual payment plan, you will receive a discount compared to monthly payments. This is a great way to enjoy all the features of our platform at a reduced cost.",
+  },
+  {
+    id: 1,
+    question: "Can I change my subscription plan at any time?",
+    answer:
+      "Absolutely! You can upgrade or downgrade your subscription at any time. Changes to your plan will take effect at the start of your next billing cycle.",
+  },
+  {
+    id: 3,
+    question: "What forms of payment do you accept?",
+    answer:
+      "We accept all major credit cards, including Visa, MasterCard, and American Express. We also accept payments through PayPal for added convenience.",
+  },
+  {
+    id: 4,
+    question:
+      "Do you offer any special pricing for educational institutions or non-profits?",
+    answer:
+      "Yes, we support educational institutions and non-profit organizations by offering special discounted pricing. Please contact us for more details and to see if you qualify.",
+  },
+  {
+    id: 5,
+    question: "What is your refund policy?",
+    answer:
+      "We offer a 30-day money-back guarantee. If you're not satisfied with our service within the first 30 days of your subscription, you can cancel your plan and receive a full refund.",
+  },
+  {
+    id: 6,
+    question: "Are there any additional fees apart from the subscription cost?",
+    answer:
+      "No, there are no hidden fees. The subscription price is all-inclusive, covering full access to our library and all features listed in your chosen plan.",
+  },
+];
